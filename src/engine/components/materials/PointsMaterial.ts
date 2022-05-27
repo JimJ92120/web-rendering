1e6be20044d715ba99c6b7c8d8dee2e425d2d326
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-05-27 20:39:53 +0700

    refactor(engine): remove unused in points material

--- src/engine/components/materials/PointsMaterial.ts
+++ src/engine/components/materials/PointsMaterial.ts
@@ -1,12 +1,8 @@
-import { BufferData } from "@/engine/types";
-
 import Material from "@/engine/components/Material";
 import Buffer from "@/engine/components/Buffer";
 
 export default class PointsMaterial extends Material {
   draw() {
-    this.setBuffer(this.vertices);
-    this.setBuffer(this.colors);
     const positionBuffer = new Buffer(this.context, this.vertices);
     const colorBuffer = new Buffer(this.context, this.colors);
 
@@ -17,11 +13,5 @@ export default class PointsMaterial extends Material {
       0,
       this.vertices.data.length / this.vertices.size
     );
-  }
-
-  setBuffer(bufferData: BufferData) {
-    const buffer = new Buffer(this.context, bufferData);
-
-    buffer.load();
   }
 }
