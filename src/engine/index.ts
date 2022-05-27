8097220cec12aebaae30d4b9eb00144ed73a90c3
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-05-28 05:28:31 +0700

    feat(engine): add other objects examples

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,39 +1,59 @@
 import { vec2, vec4 } from "@/engine/types";
 
 import Material from "@/engine/components/Material";
-import Renderer from "@/engine/Renderer";
+import Renderer from "@/engine/components/Renderer";
+import Scene from "@/engine/components/Scene";
 
 import BigPoint from "@/engine/objects/shaders/vertex/BigPoint";
+import IncreaseColor from "@/engine/objects/shaders/fragment/IncreaseColor";
 import StandardColor from "@/engine/objects/shaders/fragment/StandardColor";
+
 import Points from "@/engine/objects/materials/Points";
 import Triangles from "@/engine/objects/materials/Triangles";
+import SmallPoint from "@/engine/objects/shaders/vertex/SmallPoint";
 
 export function run(canvasId: string) {
   console.log("running...");
 
-  const renderer = new Renderer(canvasId);
+  const scene = new Scene(canvasId);
 
   const dimensions: vec2 = [800, 500];
   const clearColor: vec4 = [1.0, 0.0, 0.0, 1.0];
 
-  renderer.resize(dimensions);
-  renderer.clearColor(clearColor);
+  scene.resize(dimensions);
+  scene.clearColor(clearColor);
 
-  const bigPoint = BigPoint(renderer.context);
-  const standardColor = StandardColor(renderer.context);
+  const bigPoint = BigPoint(scene.context);
+  const smallPoint = SmallPoint(scene.context);
+  const increaseColor = IncreaseColor(scene.context);
+  const standardColor = StandardColor(scene.context);
 
-  renderer.attachShaders([bigPoint, standardColor]);
+  const renderer = new Renderer(scene);
+  renderer.attachShaders([bigPoint, increaseColor]);
+  const triangles: Material = Triangles(scene.context, renderer.program, [
+    bigPoint,
+    increaseColor,
+  ]);
 
-  const points: Material = Points(renderer.context, renderer.program, [
+  const renderer2 = new Renderer(scene);
+  renderer2.attachShaders([bigPoint, standardColor]);
+  const points: Material = Points(scene.context, renderer2.program, [
     bigPoint,
     standardColor,
   ]);
-  const triangles: Material = Triangles(renderer.context, renderer.program, [
-    bigPoint,
-    standardColor,
+
+  const renderer3 = new Renderer(scene);
+  renderer3.attachShaders([smallPoint, increaseColor]);
+  const points2: Material = Points(scene.context, renderer3.program, [
+    smallPoint,
+    increaseColor,
   ]);
 
-  renderer.addMaterials([triangles, points]);
+  renderer.addMaterials([triangles]);
+  renderer2.addMaterials([points]);
+  renderer3.addMaterials([points2]);
 
   renderer.render();
+  renderer2.render();
+  renderer3.render();
 }
---
+++ src/engine/objects/shaders/fragment/IncreaseColor.ts
@@ -0,0 +1,18 @@
+import Shader from "@/engine/components/Shader";
+
+const SOURCE = `
+  #version 100
+  precision mediump float;
+
+  varying lowp vec4 vertexColor;
+
+  void main() {
+    vec3 color = vec3(0.5, 0.5, 0.5) + vec3(vertexColor);
+
+    gl_FragColor = vec4(color, vertexColor[3]);
+  }
+`;
+
+export default function IncreaseColor(context: WebGL2RenderingContext): Shader {
+  return new Shader(context, context.FRAGMENT_SHADER, SOURCE, "a_vertexColor");
+}
